19b3a6e62817d922dd0cdcdffa9652626e38a75d
Dani <carlos99.cp88secundaria@gmail.com>
2024-09-05 16:55:59 +0200

    longitud

--- app.py
+++ app.py
@@ -2,27 +2,26 @@ import itertools
 import string
 import random
 
-
 caracteres = string.ascii_letters + string.digits
-
-
-longitud_fija = 8  
-
 
 contraseñas_generadas = set()
 
 with open('diccionario_wifi_formato.txt', 'w') as f:
-    while len(contraseñas_generadas) < 3000:
-       
-        password = ''.join(random.choices(caracteres, k=longitud_fija))
-       
+    while len(contraseñas_generadas) < 30:
+        # Longitud aleatoria entre 6 y 15
+        longitud_aleatoria = random.randint(6, 15)
+
+        # Generar contraseña con longitud aleatoria
+        password = ''.join(random.choices(caracteres, k=longitud_aleatoria))
+
+        # Verificar que cumple con las condiciones de seguridad
         if (any(c.islower() for c in password) and
             any(c.isupper() for c in password) and
             any(c.isdigit() for c in password) and
             password not in contraseñas_generadas):
-           
+
             contraseñas_generadas.add(password)
             f.write(password + '\n')
-            print(f"Contraseña {len(contraseñas_generadas)}: {password}")  
+            print(f"Contraseña {len(contraseñas_generadas)}: {password}")
 
 print("Generación completa.")
